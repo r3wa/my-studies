08a68f5c2cfa3821a9af5fec1577324921861059
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2010-09-17 10:58:05 +0000

    change javadoc for this class

--- uml/composition/src/main/java/br/com/studies/uml/composition/StatusBar.java
+++ uml/composition/src/main/java/br/com/studies/uml/composition/StatusBar.java
@@ -8,6 +8,8 @@ package br.com.studies.uml.composition;
  * this is content in relation, when the container is destroyed , so are the content.
  * 
  * @author rduarte
+ * 
+ * @see <a href='http://refcardz.dzone.com/assets/download/refcard/2bfde79c85c0c1ee91a91c273c36fc3d/rc112-010d-uml.pdf'>Refcard about UML</a>
  *
  */
 public class StatusBar {
--- uml/composition/src/main/java/br/com/studies/uml/composition/Window.java
+++ uml/composition/src/main/java/br/com/studies/uml/composition/Window.java
@@ -8,6 +8,8 @@ package br.com.studies.uml.composition;
  * This is container in relation , when the container is destroyed, so are the contents.
  * 
  * @author rduarte
+ * 
+ * @see <a href='http://refcardz.dzone.com/assets/download/refcard/2bfde79c85c0c1ee91a91c273c36fc3d/rc112-010d-uml.pdf'>Refcard about UML</a>
  *
  */
 public class Window {
