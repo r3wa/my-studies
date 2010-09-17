297c20d0e5264ae448e958009afeff68cc1f16ca
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2010-09-17 10:57:00 +0000

    change javadoc for this class

--- uml/dependency/src/test/java/br/com/studies/uml/dependency/Report.java
+++ uml/dependency/src/test/java/br/com/studies/uml/dependency/Report.java
@@ -8,6 +8,9 @@ package br.com.studies.uml.dependency;
  * This interface don't have relation with ReportPrint
  * 
  * @author rduarte
+ * 
+ * 
+ * @see <a href='http://refcardz.dzone.com/assets/download/refcard/2bfde79c85c0c1ee91a91c273c36fc3d/rc112-010d-uml.pdf'>Refcard about UML</a>
  *
  */
 public interface Report {
--- uml/dependency/src/test/java/br/com/studies/uml/dependency/ReportPrint.java
+++ uml/dependency/src/test/java/br/com/studies/uml/dependency/ReportPrint.java
@@ -5,6 +5,9 @@ package br.com.studies.uml.dependency;
  * This class have dependency with interface Report
  * 
  * @author rduarte
+ * 
+ * 
+ * @see <a href='http://refcardz.dzone.com/assets/download/refcard/2bfde79c85c0c1ee91a91c273c36fc3d/rc112-010d-uml.pdf'>Refcard about UML</a>
  *
  */
 public class ReportPrint {
