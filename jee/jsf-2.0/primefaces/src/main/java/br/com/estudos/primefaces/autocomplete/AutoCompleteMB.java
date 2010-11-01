470fb006b369e77d0902d75e8bc9f496ac795af5
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2010-11-01 22:28:02 +0000

    commit project about primefaces ( demos )

---
+++ jee/jsf-2.0/primefaces/src/main/java/br/com/estudos/primefaces/autocomplete/AutoCompleteMB.java
@@ -0,0 +1,36 @@
+package br.com.estudos.primefaces.autocomplete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class AutoCompleteMB {
+	
+	
+	private String name;
+	
+	public List<String> simpleAutoComplete(String query){
+		
+		List<String> names = new ArrayList<String>();
+		
+		names.add("Robson");
+		names.add("Robson");
+		names.add("Robson");
+		names.add("Robson");
+		
+		return names;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
