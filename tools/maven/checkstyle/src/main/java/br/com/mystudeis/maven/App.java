f24f3af659f3c496bf276b0fb1af24776d618b52
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2010-11-26 15:18:05 +0000

    commit artifacts the project about maven-checkstyle-plugin

---
+++ tools/maven/checkstyle/src/main/java/br/com/mystudeis/maven/App.java
@@ -0,0 +1,28 @@
+package br.com.mystudeis.maven;
+
+/*
+ * App
+ * 
+ * Versao: 1.0
+ *
+ * Data: 26/11/2010
+ * 
+ * Copyright (C) 2010-2012
+ * Banco Votorantim. Grupo Votorantim.
+ * Todos os direitos reservados.
+ */
+/**
+ * class App.
+ * 
+ * @author rduarte
+ */
+public class App {
+    /**
+     * Método main.
+     * 
+     * @param args
+     */
+    public static void main(final String[] args) { 
+        System.out.println("Hello World!");
+    }
+}
