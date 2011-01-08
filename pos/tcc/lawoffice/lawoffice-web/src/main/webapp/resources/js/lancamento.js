46a39745fec6779d796ae716e368ad65d6274c99
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2011-01-08 16:21:24 +0000

    fechando o editar/remover na camada web

--- pos/tcc/lawoffice/lawoffice-web/src/main/webapp/resources/js/lancamento.js
+++ pos/tcc/lawoffice/lawoffice-web/src/main/webapp/resources/js/lancamento.js
@@ -1,15 +1,8 @@
-$(function() {
-	$("table").tablesorter();
-});
-
-
-
 function oneventAddCusta(data){
 	//if ( data.status == "begin")
 		//$('#ajaxLoader').show();	
 	//else 
 	if (data.status == "success"){
-		$("#tableCustas").tablesorter();
 		$("#natureza").focus();		
 	}
 }
