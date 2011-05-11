d37ee5125fd37430cc110dfdad57f70fcedaec6c
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2011-05-11 01:16:30 +0000

    LOG4J >> commit project about log4j

---
+++ frameworks/log4j/log4j/src/main/java/br/com/mystudies/log4j/LogLevelFilterFileAppender.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package br.com.mystudies.log4j;
+
+import java.io.IOException;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * 
+ * This class is reproduction this post:
+ * <br><br>
+ * http://veerasundar.com/blog/2011/05/log4j-tutorial-writing-different-log-levels-in-different-log-files/
+ * 
+ * @author robson
+ *
+ */
+public class LogLevelFilterFileAppender extends FileAppender{
+
+	public LogLevelFilterFileAppender() {
+		super();
+	}
+
+	public LogLevelFilterFileAppender(Layout layout, String filename,
+			boolean append, boolean bufferedIO, int bufferSize)
+			throws IOException {
+		super(layout, filename, append, bufferedIO, bufferSize);
+	}
+
+	public LogLevelFilterFileAppender(Layout layout, String filename,
+			boolean append) throws IOException {
+		super(layout, filename, append);
+	}
+
+	public LogLevelFilterFileAppender(Layout layout, String filename)
+			throws IOException {
+		super(layout, filename);
+	}
+
+	@Override
+	public void append(LoggingEvent event) {
+		// TODO Auto-generated method stub
+		super.append(event);
+	}
+
+	
+	
+}
