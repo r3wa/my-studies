2f3e44940d0d1a36b578436611e20db3f11cadbd
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2011-08-05 14:38:24 +0000

    TCC >> corrindo nome do pacote.

---
+++ pos/tcc/lawoffice/lawoffice-custas/src/main/java/br/com/lawoffice/custas/lancamento/LancamentoServiceLocal.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.lawoffice.custas.lancamento;
+
+import br.com.lawoffice.dominio.Custa;
+
+/**
+ * 
+ * Interface para acesso local dos serviços de Lançamentos de custas
+ * 
+ * @author rduarte
+ *
+ */
+public interface LancamentoServiceLocal extends LancamentoService {
+
+}
