fc1b3d8cf816515427a78f7f5134f33f8dff830a
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2011-11-23 00:52:16 +0000

    DESIGN PATTERN >> initial import project about proxy.

---
+++ designpattern/proxy/src/test/java/br/com/mystudies/dp/proxy/dao/impl/DaoImpl.java
@@ -0,0 +1,7 @@
+package br.com.mystudies.dp.proxy.dao.impl;
+
+import br.com.mystudies.dp.proxy.dao.Dao;
+
+public class DaoImpl<T> implements Dao<T> {
+
+}
---
+++ designpattern/proxy/src/test/java/br/com/mystudies/dp/proxy/provider/factory/Provider.java
@@ -0,0 +1,8 @@
+package br.com.mystudies.dp.proxy.provider.factory;
+
+public enum Provider {
+
+	HIBERNATE,
+	OPEN_JPA,
+	ECLIPSE_LINK;
+}
