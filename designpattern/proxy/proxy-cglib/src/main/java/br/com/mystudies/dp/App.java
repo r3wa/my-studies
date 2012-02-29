0c5f0cbafb15f73f7aee36ad1dad7c447471bd2c
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2012-02-29 01:34:58 +0000

    DESGIN PATTERN >> import initial of project about proxy with CGLIB

---
+++ designpattern/proxy/proxy-cglib/src/main/java/br/com/mystudies/dp/App.java
@@ -0,0 +1,13 @@
+package br.com.mystudies.dp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
