30f02d450ee0dbaf842951e2076196fb519b9cdb
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2012-07-19 00:21:25 +0000

    MYSTUDIES ( Project ) >> starting developing of kanban.

--- projects/mystudies/mystudies-service/src/test/java/br/com/mystudies/service/BackLogServiceBeanTest.java
+++ projects/mystudies/mystudies-service/src/test/java/br/com/mystudies/service/BackLogServiceBeanTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +22,7 @@ import org.mockito.MockitoAnnotations;
 import br.com.mystudies.domain.entity.BackLog;
 import br.com.mystudies.domain.entity.Story;
 import br.com.mystudies.domain.entity.Theme;
+import br.com.mystudies.domain.enun.Priority;
 import br.com.mystudies.service.persistence.BackLogDAO;
 
 /**
@@ -68,14 +71,15 @@ public class BackLogServiceBeanTest {
 		assertEquals(theme.getBackLog(), backLog);
 
 	}
-	
+
 	@Test
 	public void shouldListThemesInBackLog() {
 
 		BackLog backLog = new BackLog();
-		backLog.addTheme(new Theme());
-		backLog.addTheme(new Theme());
-		backLog.addTheme(new Theme());
+		backLog.addTheme(new Theme("theme1",Priority.HEIGHT,new Date()));
+		backLog.addTheme(new Theme("theme2",Priority.HEIGHT,new Date()));
+		backLog.addTheme(new Theme("theme3",Priority.HEIGHT,new Date()));
+
 
 		when(backlogDAO.find(1L)).thenReturn(backLog);
 
