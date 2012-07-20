dcf26e10fe24794d18c33b0a6dd9b3e9c169aca7
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2012-07-20 01:50:55 +0000

    JAVA 7 ( NIO.2 ) >> finishing poc about file visitor

--- java/java-seven/nio-two/src/test/java/br/com/mystudies/niotwo/DirInformationTest.java
+++ java/java-seven/nio-two/src/test/java/br/com/mystudies/niotwo/DirInformationTest.java
@@ -11,18 +11,30 @@ import org.junit.Test;
 
 public class DirInformationTest {
 
+	private DirInformation dirInformation;
+
 	@Before
 	public void setUp() throws Exception {
+		dirInformation = new DirInformation();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		dirInformation = null;
 	}
 
 	@Test
-	public void shoudeReturnNumerOfDirs() {
-		int numberOfDir = dirInformation.getNumberOfDirs(Paths.get(""));
+	public void shoudReturnNumerOfDirs() {
+		int numberOfDir =
+				dirInformation.getNumberOfDirs(Paths.get("src/test/resources/files/"));
 		assertEquals(4, numberOfDir);
+	}
+
+	@Test
+	public void shoudReturnNumberOfFiles() {
+		int numberOfDir =
+				dirInformation.getNumberOfFiles(Paths.get("src/test/resources/files/"));
+		assertEquals(10, numberOfDir);
 	}
 
 }
