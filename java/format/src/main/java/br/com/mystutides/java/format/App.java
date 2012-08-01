88bb475b8f1384769d3b78aaa38484345cf7c76d
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2012-08-01 13:26:10 +0000

    JAVA >> import project about formation in java ( initial MessageFormat )

---
+++ java/format/src/main/java/br/com/mystutides/java/format/App.java
@@ -0,0 +1,13 @@
+package br.com.mystutides.java.format;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
