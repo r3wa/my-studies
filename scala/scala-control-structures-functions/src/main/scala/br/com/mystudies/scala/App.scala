c67a2c53ff191ad9da2540ac6e93cd2e00788987
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2012-10-31 12:40:47 +0000

    SCALA > control structures and functions continues project...

--- scala/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/App.scala
+++ scala/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/App.scala
@@ -6,10 +6,17 @@ object App {
 
   def signum( n: Int )  = { if (n > 0) 1 else if (n < 0) -1 else 0 }
 
+  def unit() = {}
+
+  def loop(n: Int) = {for(i <- 1 to n){print(i)} }
+
+
   def main(args : Array[String]) {
-    println( signum(1) )
-    println( signum(-1) )
+    println( signum(10) )
+    println( signum(-10) )
     println( signum(0) )
+    println( unit() )
+    loop(10)
   }
 
 }
