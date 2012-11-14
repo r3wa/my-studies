e59ce6fcb751522c117ba7f0b6e7706423ae21f5
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2012-11-14 00:56:19 +0000

    SCALA >> control structures and functions continues exercies. FINISHED.

--- scala/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/App.scala
+++ scala/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/App.scala
@@ -11,14 +11,45 @@ object App {
   def loop() = {for(i <- 1 to 10){print(i)} }
 
   def countdown(n: Int) { for (x <- 0 to n reverse) print(x + " ") }
+  
+  
+  def product (s: String) = {
+    
+    var product = 1L; 
+   
+    for( x <- s){
+      product = product * x toLong; // << these shortcut for this >> product= product.*(x.toLong)      
+    }
+    
+    product
+
+  }
+  
+  
+  
+  
+/*  def product (s: String):Long = {
+	  if( s.size == 1 )
+	    s toLong
+	    else
+  }*/
+  
+  
+
+  
+  
 
   def main(args : Array[String]) {
+    
     println( signum(10) )
     println( signum(-10) )
     println( signum(0) )
     println( unit() )
     loop()
     countdown(30)
+   
+    print(product("Hello"))
+    
   }
-
+  
 }
