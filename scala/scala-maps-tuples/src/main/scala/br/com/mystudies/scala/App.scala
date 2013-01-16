cd338f1b274ae109c949fa0767d953daee306237
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2013-01-16 16:59:22 +0000

    SCALA >> map and tuples exercises

--- scala/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/App.scala
+++ scala/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/App.scala
@@ -1,24 +1,19 @@
 package br.com.mystudies.scala
-
+import scala.collection.mutable.Map
 
 
 object App {
 
-  def signum( n: Int ) = { if (n > 0) 1 else if (n < 0) -1 else 0 }
+  def applyDiscount(map: Map[String,Double]) = {
+  	  for((k,v) <- map)
+  	    map(k) = v - v * 0.1
 
-  def unit() = {}
+  	    map
+  }
 
-  def loop() = {for(i <- 1 to 10){print(i)} }
-
-  def countdown(n: Int) { for (x <- 0 to n reverse) print(x + " ") }
 
   def main(args : Array[String]) {
-    println( signum(10) )
-    println( signum(-10) )
-    println( signum(0) )
-    println( unit() )
-    loop()
-    countdown(30)
+  	  print(applyDiscount( Map( "pc" -> 20.0, "tv" -> 30.0 , "dvd" -> 10.0 , "cd" -> 5.0 ) ))
   }
 
 }
