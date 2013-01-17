3e958c08d12ce3f9072b48ce7e07b1f992aeef55
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2013-01-17 10:37:14 +0000

    SCALA >> map and tuples exercises continuing

--- scala/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/App.scala
+++ scala/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/App.scala
@@ -1,5 +1,7 @@
 package br.com.mystudies.scala
 import scala.collection.mutable.Map
+import scala.io.Source
+import scala.collection.mutable.HashMap
 
 
 object App {
@@ -14,6 +16,20 @@ object App {
 
   def main(args : Array[String]) {
   	  print(applyDiscount( Map( "pc" -> 20.0, "tv" -> 30.0 , "dvd" -> 10.0 , "cd" -> 5.0 ) ))
+
+
+  	  val map = new HashMap[String,Int]
+
+  	  val words =  Source.fromFile("words.txt")
+
+
+  	  for( word <- words getLines ){
+  	    map put( word , 1  )
+  	  }
+
+  	  println(map)
+  	  println(map size)
+
   }
 
 }
