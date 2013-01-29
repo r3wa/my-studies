b2e385da23ea9d0534828f37b3fbef52cd4c0b8b
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2013-01-29 10:16:55 +0000

    SCALA >> map and tuples exercises continuing

--- scala/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/App.scala
+++ scala/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/App.scala
@@ -2,6 +2,8 @@ package br.com.mystudies.scala
 import scala.collection.mutable.Map
 import scala.io.Source
 import scala.collection.mutable.HashMap
+import scala.collection.immutable.TreeMap
+import scala.collection.mutable.LinkedHashMap
 
 
 
@@ -15,6 +17,12 @@ object App {
   }
 
 
+  def getWeekDay(day: Int) = {
+    val weekdays = LinkedHashMap( 1 -> "Monday", 2 -> "Tuesday", 3 -> "Wednesday", 4 -> "Thursday",  5 -> "Friday", 6 -> "Sartuday", 7 -> "Sunday" )
+    weekdays(day)
+  }
+  
+  
   def main(args : Array[String]) {
     
   	  println(applyDiscount( Map( "pc" -> 20.0, "tv" -> 30.0 , "dvd" -> 10.0 , "cd" -> 5.0 ) ))
@@ -32,6 +40,11 @@ object App {
   	  println(map)
   	  println(map size)
   	  println(map toMap ) // immutable map
+      println(TreeMap(map.toMap.toArray:_*) ) // mutable map to immutable map to sorted map ( immutable then  ) but I travled in (  :_* )I taked example in stackoverflow.
+      println(getWeekDay(1))
+      println(getWeekDay(2))
+      
+  	  
 
   }
 
