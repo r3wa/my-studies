e7224801665e549179f3df872ef7cc9b296646f2
robson.o.d <robson.o.d@e9df6f47-f091-bbda-6991-b925dddbb531>
2013-05-21 11:03:49 +0000

    SCALA >> import new project about packages imports

---
+++ scala/scala-packages-imports/src/main/scala/br/com/mystudies/scala/Main.scala
@@ -0,0 +1,18 @@
+package br.com.mystudies.scala
+
+object Main extends App {
+  	println(new C1);
+	println(new C2);
+	println(new C3);
+
+
+	println(randon nextInt)
+	println(randon nextDouble)
+
+	randon setSeed(1)
+
+	println(randon nextInt)
+	println(randon nextDouble)
+
+
+}
